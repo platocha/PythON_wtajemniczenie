25b5dbc3f119880c8428f8dec3044bbaca9bd9ee
Pawel Latocha <pawel.latocha1990@gmail.com>
2022-11-09 12:33:37 +0100

    Example 1 for keyword function only

---
+++ keyword_only/example_1.py
@@ -0,0 +1,19 @@
+def run_example():
+    loan_amount = int(input("Podaj wartosc kredytu: "))
+    property_value = int(input("Podaj wartosc nieruchomosci: "))
+
+    is_affordable = check_affordability(loan_amount, property_value)
+    print_results(is_affordable)
+
+def check_affordability(loan_amount, propert_value):
+    loan_to_value = loan_amount / propert_value
+    return loan_to_value <= 0.8
+
+def print_results(is_affordable):
+    if is_affordable:
+        print("Mozesz wziac kredyt!")
+    else:
+        print("Niestety nie mozesz wziac kredytu...")
+
+if __name__ == "__main__":
+    run_example()
