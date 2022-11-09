643c8d49b991746a0b7e3d0eb99c1bbd1ab4ad00
Pawel Latocha <pawel.latocha1990@gmail.com>
2022-11-09 12:38:57 +0100

    Change check_affordability to keyword only

--- keyword_only/example_1.py
+++ keyword_only/example_1.py
@@ -2,11 +2,11 @@ def run_example():
     loan_amount = int(input("Podaj wartosc kredytu: "))
     property_value = int(input("Podaj wartosc nieruchomosci: "))
 
-    is_affordable = check_affordability(loan_amount, property_value)
+    is_affordable = check_affordability(loan_amount=loan_amount, property_value=property_value)
     print_results(is_affordable)
 
-def check_affordability(loan_amount, propert_value):
-    loan_to_value = loan_amount / propert_value
+def check_affordability(*, loan_amount, property_value):
+    loan_to_value = loan_amount / property_value
     return loan_to_value <= 0.8
 
 def print_results(is_affordable):
