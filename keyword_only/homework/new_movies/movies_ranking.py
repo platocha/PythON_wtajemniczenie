695268917002b37a75908953fe7d34e8b836c4a5
Pawel Latocha <pawel.latocha1990@gmail.com>
2022-11-13 17:23:55 +0100

    Break instead of if for limiting movies to be printed

--- keyword_only/homework/new_movies/movies_ranking.py
+++ keyword_only/homework/new_movies/movies_ranking.py
@@ -1,5 +1,6 @@
 def print_top_movies(movies, *, number_of_movies = 10):
     movies_in_rate_order = sorted(movies, key=lambda single_movie: single_movie.rate, reverse=True)
     for index, movie in enumerate(movies_in_rate_order):
-        if index < number_of_movies:
-            print(f"{index+1}. {movie}")
+        if index == number_of_movies:
+            break
+        print(f"{index+1}. {movie}")
